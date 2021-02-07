9d1962aa6cb905bc45b892ff8fde40ee8bc2187d
Steve Smith <steve@kentsmiths.com>
2021-02-07 17:34:40 -0500

    Update BaseEntity.cs

--- SharedKernel/src/PluralsightDdd.SharedKernel/BaseEntity.cs
+++ SharedKernel/src/PluralsightDdd.SharedKernel/BaseEntity.cs
@@ -2,6 +2,9 @@
 
 namespace PluralsightDdd.SharedKernel
 {
+    /// <summary>
+    /// Base types for all Entities which track state using a given Id.
+    /// </summary>
     public abstract class BaseEntity<TId>
     {
         public TId Id { get; set; }
