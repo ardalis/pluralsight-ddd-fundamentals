51be24d1e582285d76225462138438a99dc8a391
Steve Smith <steve@kentsmiths.com>
2021-02-07 17:31:45 -0500

    Updating comments and attempting to publish updated nuget package

--- SharedKernel/src/PluralsightDdd.SharedKernel/Interfaces/IDomainEvent.cs
+++ SharedKernel/src/PluralsightDdd.SharedKernel/Interfaces/IDomainEvent.cs
@@ -4,6 +4,9 @@ namespace PluralsightDdd.SharedKernel.Interfaces
 {
     public interface IDomainEvent
     {
+        /// <summary>
+        /// UTC DateTime when event occurred
+        /// </summary>
         DateTime DateTimeEventOccurred { get; }
     }
 }
