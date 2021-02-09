afc0d925a345a1d1bfada7044441e09f7a2be535
Steve Smith <steve@kentsmiths.com>
2021-02-09 10:03:14 -0500

    adding placeholder tests

---
+++ FrontDesk/tests/UnitTests/Core/AggregatesEntities/ScheduleTests/Schedule_AddNewAppointment.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using Xunit;
+
+namespace UnitTests.Core.AggregatesEntities.ScheduleTests
+{
+  public class Schedule_AddNewAppointment
+  {
+    private Fixture _fixture = new Fixture();
+
+    [Fact]
+    public async Task ThrowsGivenDuplicateAppointment()
+    {
+
+    }
+    [Fact]
+    public async Task MarksConflictingAppointments()
+    {
+
+    }
+    [Fact]
+    public async Task AddsAppointmentScheduledEvent()
+    {
+
+    }
+
+  }
+}
---
+++ FrontDesk/tests/UnitTests/Core/AggregatesEntities/ScheduleTests/Schedule_DeleteAppointment.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using Xunit;
+
+namespace UnitTests.Core.AggregatesEntities.ScheduleTests
+{
+  public class Schedule_DeleteAppointment
+  {
+    private Fixture _fixture = new Fixture();
+
+    [Fact]
+    public async Task RemovesAppointment()
+    {
+
+    }
+
+    [Fact]
+    public async Task MarksConflictingAppointments()
+    {
+
+    }
+
+  }
+}
