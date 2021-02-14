6fea2a9c7d18172c255a36a680a87e7bc83e2497
ShadyNagy <info@shadynagy.com>
2021-02-14 22:00:49 +0200

    Schedule_AddNewAppointment tests added.

--- FrontDesk/tests/UnitTests/Core/AggregatesEntities/ScheduleTests/Schedule_AddNewAppointment.cs
+++ FrontDesk/tests/UnitTests/Core/AggregatesEntities/ScheduleTests/Schedule_AddNewAppointment.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoFixture;
+using FrontDesk.Core.Aggregates;
+using PluralsightDdd.SharedKernel;
 using Xunit;
 
 namespace UnitTests.Core.AggregatesEntities.ScheduleTests
@@ -7,6 +11,10 @@ namespace UnitTests.Core.AggregatesEntities.ScheduleTests
   public class Schedule_AddNewAppointment
   {
     private Fixture _fixture = new Fixture();
+
+    private readonly Guid _scheduleId = Guid.Parse("4a17e702-c20e-4b87-b95b-f915c5a794f7");
+    private readonly DateTimeRange _dateRange;
+    private readonly int _clinicId = 1;
 
     [Fact]
     public async Task ThrowsGivenDuplicateAppointment()
@@ -19,9 +27,31 @@ namespace UnitTests.Core.AggregatesEntities.ScheduleTests
 
     }
     [Fact]
-    public async Task AddsAppointmentScheduledEvent()
+    public void AddsAppointmentScheduledEvent()
     {
+      var schedule = new Schedule(_scheduleId, _dateRange, _clinicId, null);
+      var appointmentType = 1;
+      var doctorId = 2;
+      var patientId = 3;
+      var roomId = 4;
 
+      DateTime lisaStartTime = new DateTime(2021, 01, 01, 10, 00, 00);
+      DateTime lisaEndTime = new DateTime(2021, 01, 01, 11, 00, 00);
+      var lisaDateRange = new DateTimeRange(lisaStartTime, lisaEndTime);
+      var lisaTitle = "Lisa Appointment";
+      var lisaAppointment = new Appointment(appointmentType, _scheduleId, _clinicId, doctorId, patientId, roomId, lisaDateRange, lisaTitle);
+      schedule.AddNewAppointment(lisaAppointment);
+
+      DateTime mimiStartTime = new DateTime(2021, 01, 01, 12, 00, 00);
+      DateTime mimiEndTime = new DateTime(2021, 01, 01, 14, 00, 00);
+      var mimiDateRange = new DateTimeRange(mimiStartTime, mimiEndTime);
+      var mimiTitle = "Mimi Appointment";
+      var mimiAppointment = new Appointment(appointmentType, _scheduleId, _clinicId, doctorId, patientId, roomId, mimiDateRange, mimiTitle);
+      schedule.AddNewAppointment(mimiAppointment);
+
+      Assert.Equal(2, schedule.Appointments.Count());
+      Assert.False(lisaAppointment.IsPotentiallyConflicting);
+      Assert.False(mimiAppointment.IsPotentiallyConflicting);
     }
 
   }
