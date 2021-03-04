a96c66d8f32f6930390e6b408b50d1996daee359
Steve Smith <steve@kentsmiths.com>
2021-03-04 13:22:24 -0500

    Updating specification version used, adding read only repository interface

--- SharedKernel/src/PluralsightDdd.SharedKernel/Interfaces/IReadRepository.cs
+++ SharedKernel/src/PluralsightDdd.SharedKernel/Interfaces/IReadRepository.cs
@@ -2,7 +2,7 @@
 
 namespace PluralsightDdd.SharedKernel.Interfaces
 {
-  public interface IRepository<T> : IRepositoryBase<T> where T : class, IAggregateRoot
+  public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class, IAggregateRoot
   {
   }
 
