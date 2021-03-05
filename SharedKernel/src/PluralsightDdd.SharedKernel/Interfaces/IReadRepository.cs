367b07afcdfba2390bc841f06df385a80cdc9d0b
Steve Smith <steve@kentsmiths.com>
2021-03-05 14:20:40 -0500

    remove comments

--- SharedKernel/src/PluralsightDdd.SharedKernel/Interfaces/IReadRepository.cs
+++ SharedKernel/src/PluralsightDdd.SharedKernel/Interfaces/IReadRepository.cs
@@ -5,19 +5,4 @@ namespace PluralsightDdd.SharedKernel.Interfaces
   public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class, IAggregateRoot
   {
   }
-
-  //public interface IRepository
-  //{
-  //  Task<T> GetByIdAsync<T, TId>(TId id) where T : BaseEntity<TId>, IAggregateRoot;
-  //  Task<T> GetAsync<T, TId>(ISpecification<T> spec) where T : BaseEntity<TId>, IAggregateRoot;
-
-  //  Task<List<T>> ListAsync<T, TId>() where T : BaseEntity<TId>, IAggregateRoot;
-  //  Task<List<T>> ListAsync<T, TId>(ISpecification<T> spec) where T : BaseEntity<TId>, IAggregateRoot;
-
-  //  Task<int> CountAsync<T, TId>(ISpecification<T> spec) where T : BaseEntity<TId>, IAggregateRoot;
-
-  //  Task<T> AddAsync<T, TId>(T entity) where T : BaseEntity<TId>, IAggregateRoot;
-  //  Task UpdateAsync<T, TId>(T entity) where T : BaseEntity<TId>, IAggregateRoot;
-  //  Task DeleteAsync<T, TId>(T entity) where T : BaseEntity<TId>, IAggregateRoot;
-  //}
 }
