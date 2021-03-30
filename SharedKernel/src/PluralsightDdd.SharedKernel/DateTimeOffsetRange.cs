c84c3a271de3b5d30f59b70d966673d8f4fe7901
Steve Smith <steve@kentsmiths.com>
2021-03-30 16:22:27 -0400

    Adding DateTimeRangeOffset to SharedKernel

---
+++ SharedKernel/src/PluralsightDdd.SharedKernel/DateTimeOffsetRange.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Ardalis.GuardClauses;
+
+namespace PluralsightDdd.SharedKernel
+{
+  public class DateTimeOffsetRange : ValueObject
+  {
+    public DateTimeOffset Start { get; private set; }
+    public DateTimeOffset End { get; private set; }
+
+    public DateTimeOffsetRange(DateTimeOffset start, DateTimeOffset end)
+    {
+      // Ardalis.GuardClauses supports extensions with custom guards per project
+      Guard.Against.OutOfRange(start, nameof(start), start, end);
+      Start = start;
+      End = end;
+    }
+
+    public DateTimeOffsetRange(DateTimeOffset start, TimeSpan duration) : this(start, start.Add(duration))
+    {
+    }
+
+    public int DurationInMinutes()
+    {
+      return (int)Math.Round((End - Start).TotalMinutes, 0);
+    }
+
+    public DateTimeOffsetRange NewDuration(TimeSpan newDuration)
+    {
+      return new DateTimeOffsetRange(this.Start, newDuration);
+    }
+
+    public DateTimeOffsetRange NewEnd(DateTime newEnd)
+    {
+      return new DateTimeOffsetRange(this.Start, newEnd);
+    }
+
+    public DateTimeOffsetRange NewStart(DateTime newStart)
+    {
+      return new DateTimeOffsetRange(newStart, this.End);
+    }
+
+    public static DateTimeOffsetRange CreateOneDayRange(DateTime day)
+    {
+      return new DateTimeOffsetRange(day, day.AddDays(1));
+    }
+
+    public static DateTimeOffsetRange CreateOneWeekRange(DateTime startDay)
+    {
+      return new DateTimeOffsetRange(startDay, startDay.AddDays(7));
+    }
+
+    public bool Overlaps(DateTimeOffsetRange dateTimeRange)
+    {
+      return this.Start < dateTimeRange.End &&
+          this.End > dateTimeRange.Start;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+      yield return Start;
+      yield return End;
+    }
+  }
+}
---
+++ SharedKernel/src/PluralsightDdd.SharedKernel/Guards/CustomGuards.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ardalis.GuardClauses
+{
+  public static class CustomGuards
+  {
+    public static DateTimeOffset OutOfRange(this IGuardClause guardClause, DateTimeOffset input, string parameterName,  DateTimeOffset rangeFrom, DateTimeOffset rangeTo)
+    {
+      return OutOfRange<DateTimeOffset>(guardClause, input, parameterName, rangeFrom, rangeTo);
+    }
+
+    // https://github.com/ardalis/GuardClauses/blob/master/src/GuardClauses/GuardClauseExtensions.cs#L255
+    private static T OutOfRange<T>(this IGuardClause guardClause, T input, string parameterName, T rangeFrom, T rangeTo)
+    {
+      Comparer<T> comparer = Comparer<T>.Default;
+
+      if (comparer.Compare(rangeFrom, rangeTo) > 0)
+      {
+        throw new ArgumentException($"{nameof(rangeFrom)} should be less or equal than {nameof(rangeTo)}");
+      }
+
+      if (comparer.Compare(input, rangeFrom) < 0 || comparer.Compare(input, rangeTo) > 0)
+      {
+        throw new ArgumentOutOfRangeException(parameterName, $"Input {parameterName} was out of range");
+      }
+
+      return input;
+    }
+  }
+}
