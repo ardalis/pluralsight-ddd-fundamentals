ea150497c3787b06898e9f76167f0c98d309cc1a
Steve Smith <steve@kentsmiths.com>
2021-04-05 16:41:16 -0400

    clean up unused method

--- FrontDesk/src/FrontDesk.Core/SyncedAggregates/Doctor.cs
+++ FrontDesk/src/FrontDesk.Core/SyncedAggregates/Doctor.cs
@@ -17,11 +17,6 @@ namespace FrontDesk.Core.Aggregates
 
     public string Name { get; private set; }
 
-    public void UpdateName(string name)
-    {
-      Name = name;
-    }
-
     public override string ToString()
     {
       return Name.ToString();
