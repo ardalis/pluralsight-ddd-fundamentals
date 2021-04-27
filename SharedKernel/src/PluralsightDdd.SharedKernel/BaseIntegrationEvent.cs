9841441ae3eaf199569c50c70fb3705fb45403ea
Steve Smith <steve@kentsmiths.com>
2021-04-27 11:35:41 -0400

    Update IIntegrationEvent to base type

--- SharedKernel/src/PluralsightDdd.SharedKernel/BaseIntegrationEvent.cs
+++ SharedKernel/src/PluralsightDdd.SharedKernel/BaseIntegrationEvent.cs
@@ -1,12 +1,17 @@
-﻿namespace PluralsightDdd.SharedKernel.Interfaces
+﻿using System;
+using MediatR;
+
+namespace PluralsightDdd.SharedKernel
 {
   /// <summary>
   /// Integration events are used to communicate between bounded contexts and/or applications.
   /// They are often mapped from domain events in the notifying system 
   /// and sometimes to domain events in the consuming system
   /// </summary>
-  public interface IIntegrationEvent
+  public abstract class BaseIntegrationEvent : INotification
   {
+    public DateTimeOffset DateOccurred { get; protected set; } = DateTime.UtcNow;
     string EventType { get; }
+
   }
 }
