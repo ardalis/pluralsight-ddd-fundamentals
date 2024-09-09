ca44cc1c1dfffa02b17bb065fdce433c9b6dd605
Cesar Aguirre <canro91@hotmail.com>
2024-09-09 13:23:29 -0500

    Ditch Startup class from Blazor.Host

--- ClinicManagement/src/ClinicManagement.Blazor.Host/Program.cs
+++ ClinicManagement/src/ClinicManagement.Blazor.Host/Program.cs
@@ -1,26 +1,36 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ClinicManagement.Blazor.Host
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddServerSideBlazor();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UsePathBase("/");
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-          Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+  app.UseDeveloperExceptionPage();
+  app.UseWebAssemblyDebugging();
 }
+else
+{
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapRazorPages();
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToFile("index.html");
+app.Run();
